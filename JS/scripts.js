4d443e08866c80d43d4a272ddf06728df0aeb484
joão pedro <joaopedro251900@gmail.com>
2024-10-17 20:54:57 -0300

    bootstrap

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,9 +9,33 @@ window.onscroll = function() {
 };
 
 
+$(window).on('load', function () {
+    $('.filters_menu li').click(function () {
+        $('.filters_menu li').removeClass('active');
+        $(this).addClass('active');
+
+        var data = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: data
+        })
+    });
+
+    var $grid = $(".grid").isotope({
+        itemSelector: ".all",
+        percentPosition: false,
+        masonry: {
+            columnWidth: ".all"
+        }
+    })
+});
+
+// nice select
+$(document).ready(function() {
+    $('select').niceSelect();
+  });
 
 
-document.addEventListener('DOMContentLoaded', function () {
+/*document.addEventListener('DOMContentLoaded', function () {
     const filtersMenuItems = document.querySelectorAll('.filters_menu li');
     const grid = document.querySelector('.grid');
     const gridItems = document.querySelectorAll('.all');
@@ -43,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Inicializa o grid
     filterItems('*'); // Mostra todos os itens inicialmente
-});
+});*/
 
 
 
